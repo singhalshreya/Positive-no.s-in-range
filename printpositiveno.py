f7699a4380d8501f524b2fc8b741c8c113089662
Shreya Singhal <106904528+singhalshreya@users.noreply.github.com>
2022-06-22 13:12:15 +0530

    Created printpositiveno.py
    Prints all the positive numbers in the link on the screen.

---
+++ printpositiveno.py
@@ -0,0 +1,10 @@
+list1 = [12, -7, 5, 64, -14]
+list2 = [12, 14, -95, 3]
+for n in list1:
+    if n >= 0:
+       print(n, end = " ")
+print("\n")
+
+for k in list2:
+    if k >= 0:
+       print(k, end = " ")
